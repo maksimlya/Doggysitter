56d313029fbfba8395776f633bf20856266c5213
Maksim Lyashenko <maksimlya@gmail.com>
2018-08-02 23:03:18 +0300

    Testing Authentication system

--- app/src/main/java/afeka/com/doggysitter/ChooseCategory.java
+++ app/src/main/java/afeka/com/doggysitter/ChooseCategory.java
@@ -7,17 +7,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class ChooseCategory extends AppCompatActivity {
     Button logOut;
+    TextView welcomeMessage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_category);
         final FirebaseAuth auth = FirebaseAuth.getInstance();
         logOut = findViewById(R.id.log_out_btn);
+        welcomeMessage = findViewById(R.id.welcome_view);
+        welcomeMessage.append(auth.getCurrentUser().getDisplayName());
 
 
         logOut.setOnClickListener(new View.OnClickListener() {
