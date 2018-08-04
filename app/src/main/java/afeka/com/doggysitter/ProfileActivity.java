5ea5d941be882faf199fc5387a711dd1410f1f2d
Maksim Lyashenko <maksimlya@gmail.com>
2018-08-05 00:11:41 +0300

    Finished profile section

--- app/src/main/java/afeka/com/doggysitter/ProfileActivity.java
+++ app/src/main/java/afeka/com/doggysitter/ProfileActivity.java
@@ -19,8 +19,11 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
@@ -40,16 +43,18 @@ import pl.tajchert.nammu.PermissionCallback;
 
 public class ProfileActivity extends AppCompatActivity {
     final FirebaseAuth auth = FirebaseAuth.getInstance();
+    public final String fname = Objects.requireNonNull(auth.getCurrentUser()).getDisplayName() + "/profilePhoto.png";
     final FirebaseStorage firebaseStorage = FirebaseStorage.getInstance();
-    final DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+    private StorageReference storageReference = firebaseStorage.getReference(fname);
+    final DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference(Objects.requireNonNull(auth.getCurrentUser().getDisplayName()));
     private EditText dogName;
     private EditText dogSpecie;
     private EditText dogAge;
     private ImageView choosePhoto;
    // private Button resetValues;
-    public final String fname = Objects.requireNonNull(auth.getCurrentUser()).getDisplayName() + "/profilePhoto.png";
+
     private FileOutputStream outputStream;
-    private StorageReference storageReference = firebaseStorage.getReference(fname);
+
 
     private String dName;
     private String dSpecie;
@@ -63,7 +68,6 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
 
         TextView nameView = findViewById(R.id.owner_name_view);
-
         nameView.append(Objects.requireNonNull(auth.getCurrentUser()).getDisplayName());
 
         dogName = findViewById(R.id.dog_name_text);
@@ -73,6 +77,22 @@ public class ProfileActivity extends AppCompatActivity {
         Button saveToDB = findViewById(R.id.save_btn);
        // resetValues = findViewById(R.id.reset_btn);
 
+       dPhoto = BitmapFactory.decodeFile(MainActivity.PHOTO_FILE_LOCATION);
+
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                dogName.setText(dataSnapshot.child("Dog Name").getValue(String.class));
+                dogSpecie.setText(dataSnapshot.child("Dog Specie").getValue(String.class));
+                dogAge.setText(String.valueOf(dataSnapshot.child("Dog Age").getValue(Integer.class)));
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
         Bitmap bitmap = BitmapFactory.decodeFile(MainActivity.PHOTO_FILE_LOCATION);
         choosePhoto.setImageBitmap(bitmap);
 
@@ -80,7 +100,7 @@ public class ProfileActivity extends AppCompatActivity {
         saveToDB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (isEmpty(dogName) || isEmpty(dogSpecie) || isEmpty(dogAge) || dPhoto == null) {
+                if (isEmpty(dogName) || isEmpty(dogSpecie) || isEmpty(dogAge)) {
                     String str = "You can't have empty fields!!!";
                     Toast.makeText(ProfileActivity.this, str, Toast.LENGTH_SHORT).show();
                 } else {
@@ -91,9 +111,9 @@ public class ProfileActivity extends AppCompatActivity {
                     dPhoto.compress(Bitmap.CompressFormat.PNG,100,baos);
                     byte[] data = baos.toByteArray();
 
-                    mDatabase.child(Objects.requireNonNull(auth.getCurrentUser().getDisplayName())).child("Dog Name").setValue(dName);
-                    mDatabase.child(auth.getCurrentUser().getDisplayName()).child("Dog Age").setValue(dAge);
-                    mDatabase.child(auth.getCurrentUser().getDisplayName()).child("Dog Specie").setValue(dSpecie);
+                    mDatabase.child("Dog Name").setValue(dName);
+                    mDatabase.child("Dog Age").setValue(dAge);
+                    mDatabase.child("Dog Specie").setValue(dSpecie);
 
                     UploadTask uploadTask = storageReference.putBytes(data);
 
