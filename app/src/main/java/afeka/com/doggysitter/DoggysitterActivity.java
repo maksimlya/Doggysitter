7eade16f545f97db30fef99708a8679363ff1a99
Maksim Lyashenko <maksimlya@gmail.com>
2018-08-06 01:59:55 +0300

    Implemented Parks List View Added Parks Database

--- app/src/main/java/afeka/com/doggysitter/DoggysitterActivity.java
+++ app/src/main/java/afeka/com/doggysitter/DoggysitterActivity.java
@@ -1,89 +1,75 @@
 package afeka.com.doggysitter;
 
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Bitmap;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
-import com.firebase.geofire.GeoFire;
-import com.firebase.geofire.GeoLocation;
-import com.firebase.geofire.GeoQuery;
-import com.firebase.geofire.GeoQueryEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-import java.util.ArrayList;
-import java.util.Iterator;
+import java.io.FileOutputStream;
 
-public class DoggysitterActivity extends AppCompatActivity {
-    private DatabaseReference ref = FirebaseDatabase.getInstance().getReference("/Geofire");
-    private GeoFire geoFire;
+public class DoggysitterActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    private EditText x;
-    private EditText y;
-    private Button send;
-    private EditText result;
-    ArrayList<String> parksInRadius = new ArrayList<>();
+    private GoogleMap mMap;
+    private Button snapShot;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doggysitter);
-        x = findViewById(R.id.x_querry);
-        y = findViewById(R.id.y_querry);
-        send = findViewById(R.id.send_querry_btn);
-        result = findViewById(R.id.result_text);
-
-        geoFire  = new GeoFire(ref);
-
-        send.setOnClickListener(new View.OnClickListener() {
+        snapShot = (Button)findViewById(R.id.snapshot_btn);
+        snapShot.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Double a = Double.parseDouble(x.getText().toString());
-                Double b = Double.parseDouble(y.getText().toString());
+                GoogleMap.SnapshotReadyCallback callback = new GoogleMap.SnapshotReadyCallback() {
+                    Bitmap bitmap;
+                    @Override
+                    public void onSnapshotReady(Bitmap snapshot) {
+                        bitmap = snapshot;
+                        try {
+                            FileOutputStream out = new FileOutputStream("/mnt/sdcard/Screenshots/Currentlocation.png");
+                            bitmap.compress(Bitmap.CompressFormat.PNG, 90, out);
+                            Toast.makeText(DoggysitterActivity.this,"Screenshot taken", Toast.LENGTH_LONG).show();
+                        }catch (Exception e){
+                            e.printStackTrace();
+                        }
 
-
-               result.append("");
-
-                GeoQuery geoQuery = geoFire.queryAtLocation(new GeoLocation(a,b),0.3);
-
-               geoQuery.addGeoQueryEventListener(new GeoQueryEventListener() {
-                   @Override
-                   public void onKeyEntered(String key, GeoLocation location) {
-                       parksInRadius.add(key);
-                       result.append(key+"\n");
-                   }
-
-                   @Override
-                   public void onKeyExited(String key) {
-                       Toast.makeText(DoggysitterActivity.this,"Someone Exited our area!!!",Toast.LENGTH_LONG).show();
-                   }
-
-                   @Override
-                   public void onKeyMoved(String key, GeoLocation location) {
-                       Toast.makeText(DoggysitterActivity.this,"Someone moved!!!!",Toast.LENGTH_LONG).show();
-                   }
-
-                   @Override
-                   public void onGeoQueryReady() {
-                   }
-
-                   @Override
-                   public void onGeoQueryError(DatabaseError error) {
-
-                   }
-               });
+                    }
+                };
+                mMap.snapshot(callback);
             }
         });
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
 
 
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
 
-
-
+        // Add a marker in Sydney and move the camera
+        LatLng delhi = new LatLng(28, 77);
+        mMap.addMarker(new MarkerOptions().position(delhi).title("Marker in Delhi"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(delhi));
     }
 }
